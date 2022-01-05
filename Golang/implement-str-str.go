1e05c72527efbd751e53a036169722b276e232b3
mtaeo <mtaeo.aladic@gmail.com>
2022-01-05 17:50:28 +0100

    Solved 28. Implement strStr()

---
+++ Golang/implement-str-str.go
@@ -0,0 +1,18 @@
+package main
+
+func strStr(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+
+	for i := 0; i < len(haystack)-len(needle)+1; i++ {
+		if haystack[i:i+len(needle)] == needle {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	print(strStr("mississippi", "issip"))
+}
